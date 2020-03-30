e0726361e49c3fd1ad441ffe9479edb878445928
Sergio Sanabria <se_sanabria@hotmail.com>
2020-03-30 12:02:26 -0300

    API Product Controller

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Shop.Web.Data;
+    using Shop.Web.Data.Entities;
+
+    [Route("api/[Controller]")]
+
+    public class ProductsController : Controller 
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(ProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+
+        }
+
+        
+    }
+}
